c3297792a5754b38e1c57f10b324358140f823d5
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-09-21 13:29:43 +0000

    Task 9 adj

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number >= 0:
+        print(f"{number % 10}", end="")
         return (number % 10)
     else:
         number = -1 * number
+        print(f"{number % 10}", end="")
         return (number % 10)
