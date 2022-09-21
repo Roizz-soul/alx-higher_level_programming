c6aa6234747951a8a2f70062daa34743fecbfc20
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-09-21 14:02:08 +0000

    Task 12

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if b == 0:
-        return 1
-    elif b > 0:
-        for i in range(abs(b) - 1):
-            a = a * a
-    else:
-        a = 1 / a
-        for i in range(abs(b) - 1):
-            a = a * (1 / a)
-    return a
+    return (a ** b)
