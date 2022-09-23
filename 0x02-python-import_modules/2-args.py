20c68e8f321d6ce3665322e71d2d8f88b3f038db
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-09-23 09:10:28 +0000

    Task 4

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 if __name__ == "__main__":
     import sys
 
-    if len(sys.argv) == 1:
+    i = len(sys.argv) - 1
+    if i == 0:
         print("0 arguments.")
-    elif len(sys.argv) == 2:
+    elif i == 1:
         print("1 argument:")
-        print("1: {}".format(sys.argv[i]))
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
-        for h in range(len(sys.argv) - 1):
+    for h in range(i):
             print("{}: {}".format(h + 1, sys.argv[h + 1]))
---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+
+    items = dir(hidden_4)
+    for i in items:
+        if i[:2] != "__":
+            print(i)
