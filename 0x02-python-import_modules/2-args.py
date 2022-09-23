58a8ff61b0b73f5c49971dd693f04b0fab4aa851
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-09-23 09:14:44 +0000

    Task 4 adj

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
     for h in range(i):
-            print("{}: {}".format(h + 1, sys.argv[h + 1]))
+        print("{}: {}".format(h + 1, sys.argv[h + 1]))
