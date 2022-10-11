c44469c0c8ae04f97e61bbb3325eb3228e215096
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-10-11 14:08:40 +0000

    Try for task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,6 +15,11 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
-        """Instance/Object method that calculates the area of square"""
-        return self.__size ** 2
+        """Instance/Object method that calculates the area of square
+
+        Returns:
+            The area of the square
+        """
+        return (self.__size ** 2)
