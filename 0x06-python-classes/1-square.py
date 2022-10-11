bb7df996d2d9937face338a434e9d6e9b4e88438
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-10-11 13:50:08 +0000

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a class square."""
+
+
+class Square:
+    """This class is about a square"""
+    def __init__(self, size):
+        """Initializing each new square.
+
+        Args:
+            size: size of the new square
+        """
+        self.__size = size
