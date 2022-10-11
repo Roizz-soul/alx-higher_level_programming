98b5c27c44f29c1db88bfefaac5626c4971c3082
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-10-11 13:38:48 +0000

    Try 4

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Define a class square"""
-
+"""Define a class square."""
 class Square:
-    """ This class is about a square but it is empty"""
+    """This class is about a square but it is empty."""
     pass
