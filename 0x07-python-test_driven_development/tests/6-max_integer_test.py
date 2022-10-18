bb2b0516bfc9698ff94b8d6bb6bf9f4af208f035
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-10-18 10:17:33 +0000

    Task 5

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Unitest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.Testcase):
+    """Define unittests for max_integer"""
+
+    def tests_lists(self):
+        """Tests the lists for max"""
+        self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertAlmostEqual(max_integer([1, 2, 4, 3]), 4)
+        self.assertAlmostEqual(max_integer([]), None)
+        self.assertAlmostEqual(max_integer([1, 1, 1, 1]), 1)
+        self.assertAlmostEqual(max_integer([1]), 1)
+        self.assertAlmostEqual(max_integer([1.5, 2.3, 3.6, 4.2]), 4.2)
+        self.assertAlmostEqual(max_integer(["name", "is", "Book"]), "name")
+
+    if __name__ == '__main__':
+        unittest.main
