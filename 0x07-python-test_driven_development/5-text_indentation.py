30f1b16d6a11a1ad1f8c75ddfa2214d757b69869
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-10-18 09:31:02 +0000

    Task 4

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" text indentation function"""
+
+
+def text_indentation(text):
+    """prints text and replaces . ? : with a newline
+    Args:
+        text: text to be printed
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    b = 0
+    for i in text:
+        if b == 1 and i in " ":
+            continue
+        b = 0
+        print(i, end="")
+        if i in ".?:":
+            b = 1
+            print()
