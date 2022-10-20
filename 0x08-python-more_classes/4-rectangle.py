5a869c84215c3022ec08f54be781fbf188cebd7f
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-10-20 21:56:43 +0000

    Fixed errors

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -84,7 +84,7 @@ class Rectangle:
             [rect.append('#') for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append("\n")
-            return ("".join(rect))
+        return ("".join(rect))
 
     def __repr__(self):
         """representing a string
--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -119,7 +119,7 @@ class Rectangle:
             [rect.append(self.print_symbol) for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append("\n")
-            return ("".join(rect))
+        return ("".join(rect))
 
     def __repr__(self):
         """representing a string
