441aa50b176464467b3a85ef6815454e9306299f
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-10-25 04:51:51 +0000

    adj

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,10 +6,11 @@ class MyInt(int):
     """Changing some builtin methods"""
     def __eq__(self, value):
         """change == to !=
-        Returns: 
+        Returns:
             the changed thing
         """
         return self.real != value
+
     def __ne__(self, value):
         """change != to ==
         Returns:
