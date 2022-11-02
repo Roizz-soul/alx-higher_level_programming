c98a613f6c7149668f5259a9502baafc952f658b
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-11-02 11:50:46 +0000

    Task 2

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+""" A class called Rectangle that inherits from the Base class"""
+from base import Base
+
+
+class Rectangle(Base):
+    """ A rectangle class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """initialization method
+        Args:
+            width: width of the rectangle
+            height: height of the rectangle
+            x: horizontal position
+            y: vertical position
+            id: identity
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self. y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """ Getter for the width
+        Returns:
+            the width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Width Setter"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter for the height
+        Returns:
+            the height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """ Getter for x
+        Returns:
+            the value of x
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ x setter"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """ Getter for y
+        Returns:
+            the value of y
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ y setter"""
+        self.__y = value
