754e6007883146fe561253c7b17dfb10ba2ef2c8
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-11-02 12:02:02 +0000

    Task 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -31,6 +31,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Width Setter"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -44,6 +48,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """height setter"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -57,6 +65,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """ x setter"""
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__x = value
 
     @property
@@ -70,4 +82,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """ y setter"""
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
