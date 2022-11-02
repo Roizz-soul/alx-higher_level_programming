3cd27139dae7562ec2c97020a7687e25a3277e96
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-11-02 15:37:57 +0000

    Task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,6 +15,24 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """ Getter for the width
+        Returns:
+            the width
+        """
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        """Width Setter"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+        self.__height = value
+
     def __str__(self):
         """the string method
         Returns:
