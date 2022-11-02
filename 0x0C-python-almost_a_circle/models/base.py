bb9d6dddc192ed969bf22454b9b4ef3f9c27b2a1
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-11-02 16:38:37 +0000

    Task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """A class called base"""
+import json
 
 
 class Base:
@@ -19,3 +20,16 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """java script object notation
+        Args:
+            list_dictionaries: a list o dictionaries
+        Returns:
+            JSON string interpretation
+        """
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
