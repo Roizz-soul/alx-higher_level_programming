b35340c3ba355f7bcde466caa25323db26d398d7
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-11-02 14:44:47 +0000

    Task 8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -120,6 +120,29 @@ class Rectangle(Base):
             print(" " * self.__x, end="")
             print("#" * self.__width)
 
+    def update(self, *args):
+        """updating the parameters
+        Args:
+            *args: values to be changed
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
+
     def __str__(self):
         """string value
         Returns:
