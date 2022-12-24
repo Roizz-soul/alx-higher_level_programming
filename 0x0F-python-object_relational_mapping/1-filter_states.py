6197eb3223bd79ac7eec1a05c5da9a5adf023967
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-12-24 14:02:00 +0000

    Tasks 1 to 3

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Script to list all states from  a database with filter"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%'\
+                ORDER BY states.id ASC")
+    for i in cur.fetchall():
+        print(i)
+    cur.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Script to list all states from  a database with filter"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8")
+    sql = "SELECT * FROM states WHERE states.name='{}'\
+           ORDER BY states.id ASC"
+    cur = db.cursor()
+    cur.execute(sql.format(sys.argv[4]))
+    for i in cur.fetchall():
+        print(i)
+    cur.close()
+    db.close()
