f374e8bdf21c2827ba572cde55211e976ffde528
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-12-24 15:12:52 +0000

    Tah

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Script to list all states from  a database with filter"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8")
+    sql = "SELECT cities.name\
+           FROM cities JOIN states\
+           WHERE cities.state_id = states.id AND states.name=%s\
+           ORDER BY cities.id ASC"
+    cur = db.cursor()
+    cur.execute(sql, (sys.argv[4],))
+    cit = []
+    for i in cur.fetchall():
+        cit.append(i[0])
+    print(", ".join(cit))
+    cur.close()
+    db.close()
