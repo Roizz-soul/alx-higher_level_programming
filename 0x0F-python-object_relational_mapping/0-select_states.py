a050d12a689fba5255f108c461a2270840fdc30f
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-12-24 12:06:40 +0000

    corr.

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,10 +6,10 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
-        	host="localhost",
+            host="localhost",
             port=3306,
             user=sys.argv[1],
-		    passwd=sys.argv[2],
+            passwd=sys.argv[2],
             db=sys.argv[3],
             charset="utf8")
     cur = db.cursor()
