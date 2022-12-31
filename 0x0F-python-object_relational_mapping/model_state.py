ce9e9d3fdd003b13cb130a9dacd10fbe78b522f0
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-12-31 09:33:45 +0000

    6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""module defines a class called State"""
+from sqlalchemy import Column, Intger, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """creates a class state to represent a table in a database
+    Attributes:
+        __tablename__: name of the table
+        id: state id
+        name: state name
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
