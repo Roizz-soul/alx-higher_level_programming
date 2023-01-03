f6a8c6f7578895a34fa8baadbba17cf976673a6c
Oluremi Abraham <olurmeiabraham9@gmail.com>
2023-01-03 13:29:05 +0000

    11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Script that lists all objects from a database"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_row = State(name='Louisiana')
+    session.add(new_row)
+    session.commit()
+    result = session.query(State).filter(State.name == 'Louisiana').all()
+    if result:
+        for i in result:
+            print(i.id)
+    else:
+        print("Not found")
+    session.close()
