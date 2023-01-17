f473fd5c6c6727ca1af8a4b8fcc133d92ace7b81
Oluremi Abraham <olurmeiabraham9@gmail.com>
2023-01-17 20:28:55 +0000

    Task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,7 +7,7 @@ import sys
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    if sys.argc > 1:
+    if len(sys.argv) > 1:
         q = sys.argv[1]
     else:
         q = ""
