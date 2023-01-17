54bc336d40d08bde2859b8d773dfa77d86f37875
Oluremi Abraham <olurmeiabraham9@gmail.com>
2023-01-17 20:23:17 +0000

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A script to take in a URL, send a request and display a variable"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if sys.argc > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+    resp = requests.post(url, data={"q": q})
+    try:
+        result = resp.json()
+        if result == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(result.get("id"), result.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
